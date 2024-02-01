9f37e3dd28640aa86f7d29906172921ab88964ab
Boyd Querubin <boyd@ezlynkna.com>
2024-02-01 14:17:26 -0700

    first commit

---
+++ findLuckyNumbers.js
@@ -0,0 +1,13 @@
+// Write your code below this line.
+
+function luckyNumbers(n) {
+    const uniqueNumbers = [];
+    while (uniqueNumbers.length < n) {
+      const randomNumber = Math.floor(Math.random() * 10) + 1;
+      if (!uniqueNumbers.includes(randomNumber)) {
+        uniqueNumbers.push(randomNumber);
+      }
+    }
+    return uniqueNumbers;
+  }
+  
